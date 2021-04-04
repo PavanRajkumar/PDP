d6fe0c961ae3184dc3c79552a6f05c7361bf0fdc
Richard_Delwin <richarddelwin07@gmail.com>
2021-04-05 00:32:10 +0530

    added intial files and Directory structure

---
+++ speech_diagnosis/Training/NN_Training.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+class NN_Training:
+
+    def __int__(self):
+
+        data = pd.read_csv('train_data.txt', sep=",", header=None)
+
+        columns = ["Subject id", "Jitter (local)", "Jitter (local, absolute)", "Jitter (rap)", "Jitter (ppq5)",
+                        "Jitter (ddp)", "Shimmer (local)", "Shimmer (local, dB)", "Shimmer (apq3)", "Shimmer (apq5)",
+                        "Shimmer (apq11)", "Shimmer (dda)", "AC", "NTH", "HTN", "Median pitch", "Mean pitch",
+                        "Standard deviation", "Minimum pitch", "Maximum pitch", "Number of pulses", "Number of periods",
+                        "Mean period", "Standard deviation of period", "Fraction of locally unvoiced frames",
+                        "Number of voice breaks", "Degree of voice breaks", "UPDRS", "class information"]
+
+
