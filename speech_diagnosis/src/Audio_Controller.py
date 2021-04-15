d462cf50dad9638c0093f6840f96041f9f49831d
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2021-04-16 01:19:24 +0530

    Audio Processing Service complete

--- speech_diagnosis/src/Audio_Controller.py
+++ speech_diagnosis/src/Audio_Controller.py
@@ -1,7 +1,10 @@
+from Audio_Processing.feature_extraction import Feature_extraction
+from Prediction.Predict import Predict
 
-class Audio:
+class Audio_Controller:
 
-    def __init__(self):
+    def __init__(self, audio_file_location):
 
-        audio = None
-        signals = None
+        self.audio_file_location = audio_file_location
+        self.features_extraction = None
+        
