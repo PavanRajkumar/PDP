38ebd6d15af0124c13115785045a659d78b673ca
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2021-04-16 01:34:36 +0530

    Audio Controller added

--- speech_diagnosis/src/Audio_Controller.py
+++ speech_diagnosis/src/Audio_Controller.py
@@ -7,4 +7,24 @@ class Audio_Controller:
 
         self.audio_file_location = audio_file_location
         self.features_extraction = None
-        
+        self.features = None
+        self.predict_object = None
+        self.prediction = None
+
+    def process_audio(self):
+        self.features_extraction = Feature_extraction(self.audio_file_location)
+        self.features = self.features_extraction.get_features_as_numpy()
+
+    def predict_PD_diagnosis(self, model_name="NN"):
+        self.predict_object = Predict(model_name=model_name)
+        self.prediction = self.predict_object.get_prediction(data=self.features)
+
+        return self.prediction
+
+
+if __name__ == "__main__":
+    audio = Audio_Controller("..//data//audio_morph1Male.wav")
+    audio.process_audio()
+    res = audio.predict_PD_diagnosis()
+
+    print(res)
